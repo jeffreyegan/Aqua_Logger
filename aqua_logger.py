04ae4cb42e2ba1a1c002e85fbdb762f2afbfd740
Jeffrey Egan <jeffrey.a.egan@gmail.com>
2019-06-04 15:42:28 -0400

    GUI improvements, and writing measurement data to sqlite

--- aqua_logger.py
+++ aqua_logger.py
@@ -1,34 +1,45 @@
-import sys
-import os
-from PyQt5 import QtWidgets, uic
+import sys, os
+from PyQt5 import QtWidgets, QtCore, uic
 import sqlite3
+#from datetime import datetime
+from aqua_logger_gui import Ui_MainWindow
 
-qt_gui_file = "gui_main.ui"
+
+class mywindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(mywindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.update_time()  # Set datetime field in the GUI on startup
+        self.connect_to_database()  # Connect to the SQLite db file
+        self.ui.submit_parameters_button.clicked.connect(self.update_parameters)  # Submit Button
+
+    def update_time(self):
+        now = QtCore.QDateTime.currentDateTime()
+        self.ui.p_datetime.setDateTime(now)
+
+    def update_parameters(self):
+        self.ui.p_ph.setValue(7.9)
+        self.update_time()
+        self.add_measurement()
+
+    def connect_to_database(self):
+        db_file = "aqua_data.db"
+        self.conn = sqlite3.connect(db_file)
+        self.cur = self.conn.cursor()
+
+    def add_measurement(self):
+        dt_string = self.ui.p_datetime.dateTime().toString(self.ui.p_datetime.displayFormat())  # mm/dd/yy hh:mm"
+        q = ("INSERT INTO measurements (tank_id, measurement_time, method, temp_f, temp_c, pH, ammonia, nitrite, nitrate, copper, tds, gh, kh) " + 
+        "VALUES (1, \""+dt_string+"\", \""+str(self.ui.p_method.currentText())+"\", "+str(self.ui.p_tempf.value())+", "+str((self.ui.p_tempf.value()-32.0)*5.0/9.0)+", "+str(self.ui.p_ph.value())+", "+str(self.ui.p_ammonia.value())+", "+str(self.ui.p_nitrite.value())+", "+str(self.ui.p_nitrate.value())+", "+str(self.ui.p_cu.value())+", "+str(self.ui.p_tds.value())+", "+str(self.ui.p_gh.value())+", "+str(self.ui.p_kh.value())+")")
+        print(q)
+        self.cur.execute(q)
+        self.conn.commit()
+
+
 
 app = QtWidgets.QApplication([])
-win = uic.loadUi(qt_gui_file)
-win.show()
+application = mywindow()
+application.show()
 sys.exit(app.exec())
 
-
-
-
-
-'''Ui_MainWindow, QtBaseClass = uic.loadUi(qt_gui_file)
-
-
-class AquaLogger(QtGui.QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        QtGui.QMainWindow.__init__(self)
-        Ui_MainWindow.__init__(self)
-        self.setupUi(self)
-        self.submit_parameters_button.clicked.connect(self.Update_Parameters)
-
-    def Update_Parameters(self):
-        return
-
-if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
-    window = MyApp()
-    window.show()
-    sys.exit(app.exec_())'''
